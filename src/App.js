fb8667299be1ee817ceea185ccf5e7efe4876a66
AbuzarZaidi <abuzarzaidi947@gmail.com>
2021-09-20 18:23:26 -0800

    Now we can push on local stoarge.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import NavBar from './Components/NavBar'
 import Todos from './Components/Todos'
 function App() {
+
+
   return (
     <>
 <NavBar/>
--- src/Components/Todos.jsx
+++ src/Components/Todos.jsx
@@ -1,11 +1,17 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import Todo from './Todo';
 import './Todos.css'
 const Todos = () => {
+    let initTodo;
+  if (localStorage.getItem("todos") === null) {
+    initTodo = [];
+  } else {
+    initTodo = JSON.parse(localStorage.getItem("todos"));
+  }
     const [flag,setFlag]=useState(false);
     const [checkEmpty,setCheckEmpty]=useState(false);
     const [inputTodo,setInputTodo]=useState("+ Add Todo here.");
-    const [showTodos,setShowTodos]=useState([]);
+    const [showTodos,setShowTodos]=useState(initTodo );
     const removeplaceHolder=()=>{
         if(flag===false){
             setInputTodo(" ")
@@ -19,9 +25,7 @@ const Todos = () => {
     }
     const onClickHandler=()=>{
       if(checkEmpty===true){
-        setShowTodos((oldArray)=>{
-            return [...oldArray,inputTodo];
-        })
+        setShowTodos([...showTodos, inputTodo]);
       }
       setCheckEmpty(false);
         setInputTodo("+ Add Todo here.");
@@ -33,24 +37,28 @@ const Todos = () => {
                 return index!=id;
             })
         })
+        localStorage.setItem("todos", JSON.stringify(showTodos));
     }
-    return (
-    <div>
-     <br />
-        <div className="centerInput">
-        <input type="text" id="inputTodo" value={inputTodo}  onClick={removeplaceHolder} onChange={onChangeHandler}/>
-        <button id="addButton" onClick={onClickHandler}>+</button>
-        </div>
-        <br />
+    useEffect(() => {
+        localStorage.setItem("todos", JSON.stringify(showTodos));
+      }, [showTodos]);
+      return (
         <div>
-        <ul className="list-group list-group-numbered" id="showTodos">
-           {showTodos.map((arrEle,index)=>{
-return( <Todo todos={arrEle} deleteTodo={deleteHandler} id={index} key={index} />)
-           })}
-            </ul>
+         <br />
+            <div className="centerInput">
+            <input type="text" id="inputTodo" value={inputTodo}  onClick={removeplaceHolder} onChange={onChangeHandler}/>
+            <button id="addButton" onClick={onClickHandler}>+</button>
+            </div>
+            <br />
+            <div>
+            <ul className="list-group list-group-numbered" id="showTodos">
+               {showTodos.map((arrEle,index)=>{
+    return( <Todo todos={arrEle} deleteTodo={deleteHandler} id={index} key={index} />)
+               })}
+                </ul>
+            </div>
         </div>
-    </div>
-    )
-}
-
-export default Todos
+        )
+    }
+    
+    export default Todos
